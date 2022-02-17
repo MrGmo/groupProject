9523fa3f1de88f6ff5113388495f24401a60db42
James Kelsey <jamesmskelsey@gmail.com>
2022-02-17 09:47:42 -0800

    new component to clean up nav bar

--- frontend/recipe-app/src/components/NavBar.js
+++ frontend/recipe-app/src/components/NavBar.js
@@ -1,91 +1,37 @@
-
-import { useNavigate } from 'react-router-dom'
-import { AppBar, Toolbar, Button, Box } from '@mui/material';
-
+import { useNavigate } from "react-router-dom";
+import { AppBar, Toolbar, Button, Box } from "@mui/material";
+import NavButton from "./buttons/NavButton";
 
 const NavBar = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
+  return (
+    <Box sx={{ flexGrow: 2 }}>
+      <AppBar className="nav-bar" elevation={0} position="static">
+        <Toolbar className="toolbar" sx={styles.toolbar}>
+          <NavButton variant="title" to="/">
+            FRIENDS_COOK
+          </NavButton>
+          <NavButton to="/recipes">YOUR RECIPES</NavButton>
+          <NavButton to="/shopping_list">SHOPPING LIST</NavButton>
+          <NavButton to="/friends">FRIENDS</NavButton>
 
-        <Box sx={{ flexGrow: 2 }}>
-        <AppBar className="nav-bar" elevation={0}  position="static">
-          <Toolbar className="toolbar" sx={{ display: "flex", justifyContent: "space-between", alignItems: 'center'}}>
-            <Button
-              size="large"
-              edge="start"
-              color="primary"
-              aria-label="menu"
-              
-              // sx={{  mr: 2 }}
-              onClick={() => {
-                navigate("/");
-              }}
-              sx={{fontSize: '30px', fontWeight: 'bold'}}
-            >
-            Friends_Cook
-            </Button>
-              <Button  size="large" sx={{ m: 1, color: '#05386b', fontWeight: 'bold'}}>
-                  YOUR RECIPES
-              </Button>
-              <Button  size="large" sx={{ m: 1, color: '#05386b', fontWeight: 'bold'}}>
-                 SHOPPING LIST
-              </Button>
-              <Button  size="large" sx={{ m: 1, color: '#05386b', fontWeight: 'bold'}}>
-                  FRIENDS
-              </Button>
-            
-              <Box sx={{fontWeight: 'bold'}}>
-                <Button
-                  size="large"
-                  sx={{ m: 1, color: '#05386b', fontWeight: 'bold'}}
-                //   onClick={() => {
-                //     navigate("/signup");
-                //   }}
-                  color="inherit"
-                >
-                  Sign Up
-                </Button>
-                <Button
-                  sx={{ m: 1, color: '#05386b', fontWeight: 'bold' }}
-                //   onClick={() => {
-                //     navigate("/login");
-                //   }}
-                  color="inherit"
-                >
-                  Login
-                </Button>
-              </Box>
-        
-            {/* {user && (
-              <Box>
-                <Button 
-                  sx={{ m: 1, color: '#05386b', fontWeight: 'bold' }}
-                 
-                //   onClick={() => {
-                //     navigate(`user/${user.id}`);
-                //   }}
-                  color="inherit"
-                >
-                  My Profile
-                </Button>
-                <Button sx={{ m: 1, color: '#05386b', fontWeight: 'bold'  }} onClick={() => console.log('LOGOUT')} color="inherit">
-                  Logout
-                </Button>
-              </Box>
-            )} */}
-          </Toolbar>
-        </AppBar>
-      </Box>
+          <Box>
+            <NavButton to="/signup">SIGN UP</NavButton>
+            <NavButton to="/login">LOGIN</NavButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
 
+const styles = {
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+};
 
-
-
-    )
-
-
-
-}
-
-
-export default NavBar
+export default NavBar;
